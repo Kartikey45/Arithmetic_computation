8b47d4b7f319c6522b238b1a4d9f9dc4da3e2e94
Kartikey45 <singhkartikey45@gmail.com>
2020-02-17 12:30:18 +0530

    ADDED:Store results in dictionary for every computation

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -7,8 +7,19 @@ read -p "Enter the value of a :" number1
 read -p "Enter the value of b :" number2
 read -p "Enter the value of c :" number3
 
-#ARITHMETIC OPERATION
-answer=$(echo "scale=2;$number1%$number2+$number3"|bc)
+#ARITHMETIC OPERATIONS
+answer1=$(echo "scale=2;$number1+$number2*$number3"|bc)
+answer2=$(echo "scale=2;$number1*$number2+$number3"|bc)
+answer3=$(echo "scale=2;$number3+$number1/$number2"|bc)
+answer4=$(echo "scale=2;$number1%$number2+$number3"|bc)
 
-#DISPLAY THE RESULT
-echo $answer
+#STORE ANSWERS IN THE DICTIONARY
+declare -A answersDictionary
+
+answersDictionary[answer1]=$answer1
+answersDictionary[answer2]=$answer2
+answersDictionary[answer3]=$answer3
+answersDictionary[answer4]=$answer4
+
+#DISPLAY ANSWERS IN THE DICTIONARY
+echo "answers in the dictionary : ${answersDictionary[@]}"
