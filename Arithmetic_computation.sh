e54657b64c42392c6c1e0e01a480f15d4414bbf7
Kartikey45 <singhkartikey45@gmail.com>
2020-02-17 11:16:05 +0530

    ADDED:Arithmetic calculation

--- Arithmetic_computation.sh
+++ Arithmetic_computation.sh
@@ -3,9 +3,12 @@
 echo "Welcome to Arithmetic Computation"
 
 #TAKE USER INPUT 
-read -p "Enter the first value :" number1
-read -p "Enter the second value :" number2
-read -p "Enter the third value :" number3
+read -p "Enter the value of a :" number1
+read -p "Enter the value of b :" number2
+read -p "Enter the value of c :" number3
 
-#DISPLAY THE USER INPUTS
-echo $number1 $number2 $number3
+#ARITHMETIC OPERATION
+answer=$(echo "scale=2;$number1+$number2*$number3"|bc)
+
+#DISPLAY THE RESULT
+echo $answer
